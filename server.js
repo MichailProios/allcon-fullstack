7ed31288a1f5d54eda256900056f28bcddde6c4a
Allcon Administrator <admin@allconcontracting.com>
2022-11-30 00:58:49 -0500

    changes

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const morgan = require("morgan");
 const https = require("https");
 const fs = require("fs");
 const { createRequestHandler } = require("@remix-run/express");
-const { dirname } = require("path");
 
 const BUILD_DIR = path.join(__dirname, "build");
 
@@ -62,11 +61,6 @@ https.createServer(httpsOptions, app).listen(443, () => {
 });
 
 function purgeRequireCache() {
-  // purge require cache on requests for "server side HMR" this won't let
-  // you have in-memory objects between requests in development,
-  // alternatively you can set up nodemon/pm2-dev to restart the server on
-  // file changes, but then you'll have to reconnect to databases/etc on each
-  // change. We prefer the DX of this, so we've included it for you by default
   for (const key in require.cache) {
     if (key.startsWith(BUILD_DIR)) {
       delete require.cache[key];
